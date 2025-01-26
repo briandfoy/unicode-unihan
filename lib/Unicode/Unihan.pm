d75651bc14cf4f6d5b9c1e30c44454a066fb429b
brian d foy <briandfoy@pobox.com>
2025-01-26 17:11:57 -0500

    update email address

--- lib/Unicode/Unihan.pm
+++ lib/Unicode/Unihan.pm
@@ -163,7 +163,7 @@ Unicode, Inc.
 
 =head1 COPYRIGHT AND LICENSE
 
-This module is currently maintained by brian d foy (C<bdfoy@cpan.org>)
+This module is currently maintained by brian d foy (C<briandfoy@pobox.com>)
 for legacy applications.
 
 =over 2
